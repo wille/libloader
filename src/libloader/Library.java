099c58247abfc342aef48e00b9d61ee3215f8c85
redpois0n <red@cmail.nu>
2015-06-18 20:13:19 +0200

    fields and equals()

--- src/libloader/Library.java
+++ src/libloader/Library.java
@@ -1,5 +1,26 @@
 package libloader;
 
+import com.redpois0n.oslib.Arch;
+import com.redpois0n.oslib.OperatingSystem;
+
 public class Library {
 
+	private OperatingSystem type;
+	private Arch architecture;
+	
+	public Library(OperatingSystem type, Arch architecture) {
+		this.type = type;
+		this.architecture = architecture;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Library) {
+			Library lib = (Library) o;
+			
+			return lib.type.equals(this.type) && lib.architecture.equals(this.architecture);
+		}
+		
+		return this == o;
+	}
 }
