429ea759f0c007cc49847b05945080f0f57962a2
redpois0n <red@cmail.nu>
2015-06-18 20:47:27 +0200

    FileLibrary

---
+++ src/libloader/FileLibrary.java
@@ -0,0 +1,22 @@
+package libloader;
+
+import java.io.File;
+
+import com.redpois0n.oslib.Arch;
+import com.redpois0n.oslib.OperatingSystem;
+
+/**
+ * Load library from {@link java.io#File} path
+ */
+public class FileLibrary extends Library {
+
+	public FileLibrary(File file, OperatingSystem type, Arch architecture) {
+		super(file.getAbsolutePath(), type, architecture);
+	}
+
+	@Override
+	public void load() {
+		System.load(path);
+	}
+
+}
