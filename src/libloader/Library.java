823f3c8a2ae50aefbcb249e6871ea1f2b0f6c270
redpois0n <red@cmail.nu>
2015-06-18 20:36:12 +0200

    more shit

--- src/libloader/Library.java
+++ src/libloader/Library.java
@@ -3,15 +3,20 @@ package libloader;
 import com.redpois0n.oslib.Arch;
 import com.redpois0n.oslib.OperatingSystem;
 
-public class Library {
+public abstract class Library {
 
+	protected String path;
+	
 	private OperatingSystem type;
 	private Arch architecture;
 	
-	public Library(OperatingSystem type, Arch architecture) {
+	public Library(String path, OperatingSystem type, Arch architecture) {
+		this.path = path;
 		this.type = type;
 		this.architecture = architecture;
 	}
+	
+	public abstract void load();
 	
 	@Override
 	public boolean equals(Object o) {
