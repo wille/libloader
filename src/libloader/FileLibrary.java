8ee4c00cf0740946cae1af027db11581c9dfb3f9
redpois0n <red@cmail.nu>
2015-06-21 00:51:58 +0200

    FileLibrary from string

--- src/libloader/FileLibrary.java
+++ src/libloader/FileLibrary.java
@@ -9,7 +9,11 @@ import com.redpois0n.oslib.OperatingSystem;
  * Load library from {@link java.io#File} path
  */
 public class FileLibrary extends Library {
-
+	
+	public FileLibrary(String file, OperatingSystem type, Arch architecture) {
+		super(file, type, architecture);
+	}
+		
 	public FileLibrary(File file, OperatingSystem type, Arch architecture) {
 		super(file.getAbsolutePath(), type, architecture);
 	}
